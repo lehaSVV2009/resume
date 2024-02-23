ff5ddccee91af64f283ec265f89688f218a2694b
Aliaskei Saroka <lehasvv2009@gmail.com>
2024-02-23 23:25:28 +0300

    Upgrade to mui v5 skills section

--- src/components/PrettyJsonObject.js
+++ src/components/PrettyJsonObject.js
@@ -1,35 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./PrettyJson.scss";
 
-export default class PrettyJsonObject extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      entries: this.props.object ? Object.entries(this.props.object) : []
-    };
+const PrettyJsonObject = ({ object }) => {
+  if (!object) {
+    return <span />;
   }
 
-  render() {
-    const { object } = this.props;
+  const entries = Object.entries(object);
 
-    if (!object) {
-      return <span />;
-    }
+  return (
+    <span>
+      {entries.map((entry, index) => (
+        <span key={index}>
+          <li>&#123; </li>
+          <li className="row">
+            "<span className="json-code-key">{entry[0]}</span>" : "
+            <span className="json-code-value">{entry[1]}</span>"
+          </li>
+          <li>&#125;,</li>
+        </span>
+      ))}
+    </span>
+  );
+};
 
-    return (
-      <span>
-        {this.state.entries.map((entry, index) => (
-          <span key={index}>
-            <li>&#123; </li>
-            <li className="row">
-              "<span className="json-code-key">{entry[0]}</span>" : "
-              <span className="json-code-value">{entry[1]}</span>"
-            </li>
-            <li>&#125;,</li>
-          </span>
-        ))}
-      </span>
-    );
-  }
-}
+export default PrettyJsonObject;
